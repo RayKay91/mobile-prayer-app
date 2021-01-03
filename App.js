42bcff7255c5b51841b16f24d6b369041578e731
Rayhaan Khalid <rkhalid@me.com>
2021-01-03 15:20:33 +0000

    add styling to refresh indicator, remove console log from Anchor

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import { ScrollView, StyleSheet, Text, View, SafeAreaView, RefreshControl} from 'react-native';
-import getDate from './utils/getDate'
+//components
 import Table from './components/table'
 import Tweets from './components/tweets'
+import Anchor from './components/anchor'
+//utility functions
 import wait from './utils/wait'
+import getDate from './utils/getDate'
 
 
 
@@ -47,7 +50,7 @@ export default function App() {
     <ScrollView 
     style={styles.scrollContainer} showsVerticalScrollIndicator={false}
     refreshControl={
-      <RefreshControl refreshing={showRefresh} onRefresh={onRefresh} />
+      <RefreshControl refreshing={showRefresh} onRefresh={onRefresh} color={['rgb(161, 43, 110)']} tintColor={'rgb(161, 43, 110)'}/>
     }
     >
 
@@ -60,6 +63,8 @@ export default function App() {
       <Text style={styles.subHeading}>Latest Tweets and Announcements</Text>
 
       <Tweets refreshing={refreshing}/>
+
+      <Anchor style={styles.donate} href='https://www.totalgiving.co.uk/appeal/wisemasjidcovid19/donate'>Donate</Anchor>
       </ScrollView>
     </View>
     </SafeAreaView>
@@ -101,6 +106,20 @@ const styles = StyleSheet.create({
   refreshNotice: {
     textAlign: 'center',
     color: '#888'
+  },
+  donate: {
+    backgroundColor: 'rgb(161, 43, 110)',
+    padding: 15,
+    textAlign: 'center',
+    marginVertical: 20,
+    marginHorizontal: '33%',
+    color: 'white',
+    width: '30%',
+    fontWeight:'bold',
+    borderRadius: 7,
+    borderWidth: 1,
+    borderColor: 'rgb(161, 43, 110)',
+    overflow: 'hidden'
   }
 });
 
--- components/anchor.js
+++ components/anchor.js
@@ -2,15 +2,14 @@ import React from 'react'
 import { StyleSheet, Text } from 'react-native';
 import * as Linking from 'expo-linking';
 
-export default function Anchor({href, children}){
+export default function Anchor({href, children, style}){
   const handlePress = () => {
     Linking.openURL(href);
-    console.log('pressed')
   };
 
 
     return (
-      <Text style={styles.text} onPress={handlePress}>
+      <Text style={[styles.text, style]} onPress={handlePress}>
         {children}
       </Text>
     );
