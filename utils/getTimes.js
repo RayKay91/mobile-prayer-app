aa0959c65894cd10a3bdbc90445ec26f34476369
Rayhaan Khalid <rkhalid@me.com>
2021-01-06 12:47:19 +0000

    change GET api endpoint

--- utils/getTimes.js
+++ utils/getTimes.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 
 export default async function getTimes(){
 
-    const response = await axios.get('https://wiseprayertimes.herokuapp.com')
+    const response = await axios.get('https://wiseprayertimes.herokuapp.com/getPrayerTimes')
 
     const times = [response.data.prayerTimes, response.data.jamaaTimes]
 
