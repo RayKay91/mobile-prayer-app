228d0892cf83693771c64eb63883c32901d4bde1
Rayhaan Khalid <rkhalid@me.com>
2021-03-10 15:35:47 +0000

    fix 00 + 5 = 5 not 05 bug

--- utils/timeAdjuster.js
+++ utils/timeAdjuster.js
@@ -6,18 +6,20 @@ function timeAdjuster( time, adjustmentMins ) {
     const minutes = +cleanedTime.substring( 2 );
 
     let adjustedMins = minutes + adjustmentMins;
+    console.log( adjustedMins )
 
     const maxMins = 59;
     if ( adjustedMins > maxMins ) {
+        console.log( 'running timeAdjuster max reset' );
         adjustedMins -= 60;
         hours += 1;
 
-        if ( adjustedMins < 10 ) {
-            adjustedMins = '0' + adjustedMins;
-        }
-        if ( hours < 10 ) {
-            hours = '0' + hours;
-        }
+    }
+    if ( adjustedMins < 10 ) {
+        adjustedMins = '0' + adjustedMins;
+    }
+    if ( hours < 10 ) {
+        hours = '0' + hours;
     }
     const adjustedTime = hours + ':' + adjustedMins
 
