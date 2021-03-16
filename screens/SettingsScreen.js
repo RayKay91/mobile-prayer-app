eba1783c14cf1a0bbcc6f6970c5ef9c302fe6c67
Rayhaan Khalid <rkhalid@me.com>
2021-03-16 22:48:14 +0000

    center privacy policy

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -10,9 +10,9 @@ const SettingScreen = () => {
             <Text style={ { marginTop: 30, fontWeight: 'bold', fontSize: 22, color: '#333', marginLeft: 11 } }>Notifications</Text>
             <Text style={ { marginTop: 5, marginBottom: 10, fontSize: 14, paddingHorizontal: 11, color: '#4a4a4a' } }>Toggle the switches to select notifications for prayers.</Text>
             {prayers.map( ( prayer, i ) => <NotificationSwitch key={ i } idx={ i } prayerName={ prayer } /> ) }
-            <View style={ { marginLeft: 30, marginRight: 30, justifyContent: 'center', position: 'absolute', bottom: 15 } }>
-                <AnchorTag style={ { color: '#A12B6E', textAlign: 'center' } } url={ 'https://wise-web.org/privacy-policy/#android-app' }>Privacy Policy</AnchorTag>
-                <Text style={ { color: '#999', textAlign: 'center' } }>The privacy policy you agree to by using the app</Text>
+            <View style={ { alignSelf: 'center', justifyContent: 'center', position: 'absolute', bottom: 15 } }>
+                <AnchorTag style={ { color: '#A12B6E', textAlign: 'center', fontSize: 13 } } url={ 'https://wise-web.org/privacy-policy/#android-app' }>Privacy Policy</AnchorTag>
+                <Text style={ { color: '#999', textAlign: 'center', fontSize: 12 } }>The privacy policy you agree to by using the app</Text>
             </View>
         </View>
     )
