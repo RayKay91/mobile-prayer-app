4ad32fae13efe30316856eb1e3d30ca0815a1cd5
Rayhaan Khalid <rkhalid@me.com>
2021-03-16 11:13:54 +0000

    change date algorithms for getting correct date

--- utils/getDate.js
+++ utils/getDate.js
@@ -1,9 +1,15 @@
 const dayjs = require( 'dayjs' )
-function getDate() {
-    const gregorianDate = dayjs().format( 'dddd D MMM YYYY' )
 
-    return gregorianDate
 
+function getDate( showTmrwDate ) {
+    let date = dayjs();
+    const gregorianDate = dayjs().format( 'dddd D MMM YYYY' );
+
+    if ( showTmrwDate ) {
+        date = dayjs().add( 1, 'd' );
+        return date.format( 'dddd D MMM YYYY' );
+    }
+    return gregorianDate;
 }
 
 function calculateHijriDate( { StartDate, Month, Year } ) {
@@ -18,10 +24,23 @@ function calculateHijriDate( { StartDate, Month, Year } ) {
     return `${ elapsedHijriDays } ${ Month } ${ Year } AH`;
 }
 
+function tmrwHijriDate( currHijriDate ) {
+
+    let [ date, month, ...year ] = currHijriDate.split( ' ' );
+
+    console.log( date, month, year );
+    date += 1;
+    month = date > 30 ? 'Next Hijri Month' : month;
+    date = date > 30 ? 1 : date;
+
+    const dateElements = [ date, month, ...year ];
+
+    return dateElements.join( ' ' );
+}
 
 
 
 
 
 
-module.exports = { getDate, calculateHijriDate }
+module.exports = { getDate, calculateHijriDate, tmrwHijriDate }
