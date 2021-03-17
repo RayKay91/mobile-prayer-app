77c088298a5ea1834b697c818dda736ba0a89e74
Rayhaan Khalid <rkhalid@me.com>
2021-03-17 13:12:10 +0000

    remove some tafasir

--- screens/QuranScreen.js
+++ screens/QuranScreen.js
@@ -1,6 +1,6 @@
 import { useFocusEffect } from '@react-navigation/native'
-import React, {useState, useCallback} from 'react'
-import {SafeAreaView, Text} from 'react-native'
+import React, { useState, useCallback } from 'react'
+import { SafeAreaView, Text } from 'react-native'
 import AutoHeightWebView from 'react-native-autoheight-webview'
 
 const customCSS = `
@@ -59,25 +59,47 @@ div.row.verse-actions-below > div.col-md-1.order-1.order-md-0 > div > a:nth-chil
     display: none !important
 }
 
+/* removal of some tafaaseer */
+
+div[data-controller="tafsir-modal"] > div {
+    display: none !important
+}
+
+#tafsir-results > li:nth-child(1),
+#tafsir-results > li:nth-child(2),
+#tafsir-results > li:nth-child(3),
+#tafsir-results > li:nth-child(4),
+#tafsir-results > li:nth-child(5),
+#tafsir-results > li:nth-child(6),
+#tafsir-results > li:nth-child(8),
+#tafsir-results > li:nth-child(10),
+#tafsir-results > li:nth-child(12),
+#tafsir-results > li:nth-child(14),
+#tafsir-results > li:nth-child(15),
+#tafsir-results > li:nth-child(16),
+#tafsir-results > li:nth-child(17) 
+{
+    display: none !important;
+}
 
 `
 
-export default function QuranScreen(){
-    
-   
-    
+export default function QuranScreen() {
 
 
-    return(
-        <SafeAreaView style={{flex: 1}}>
-             <AutoHeightWebView
-                    customStyle={customCSS}
-                    containerStyle={{flex: 1, overflow: 'visible'}}
-                    originWhiteList={['https://quran.com/*']}
-                    source={{uri: 'https://quran.com'}}
-                />
+
+
+
+    return (
+        <SafeAreaView style={ { flex: 1 } }>
+            <AutoHeightWebView
+                customStyle={ customCSS }
+                containerStyle={ { flex: 1, overflow: 'visible' } }
+                originWhiteList={ [ 'https://quran.com/*' ] }
+                source={ { uri: 'https://quran.com' } }
+            />
         </SafeAreaView>
-        
+
     )
 }
 
