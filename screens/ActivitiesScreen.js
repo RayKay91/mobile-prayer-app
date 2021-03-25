cf25843f402417c5f6aa127c7f548d544aea63f9
Rayhaan Khalid <rkhalid@me.com>
2021-03-25 20:03:05 +0000

    implement 'bodge' auto scroll on activities screen

--- screens/ActivitiesScreen.js
+++ screens/ActivitiesScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect, useRef } from 'react';
 import { useFocusEffect } from '@react-navigation/native';
 import { SafeAreaView, SectionList } from 'react-native'
 import ActivityComponent from '../components/activityComponent'
@@ -6,8 +6,11 @@ import Loading from '../components/loading'
 import getActivitiesData from '../utils/getActivitiesData';
 import ActivityDay from '../components/activityDay'
 
+
 export default function ActivitiesScreen() {
     const [ activitiesData, setActivitiesData ] = useState( null )
+    const sectionList = useRef()
+    const day = new Date().getDay()
 
     useFocusEffect(
         useCallback(
@@ -21,71 +24,82 @@ export default function ActivitiesScreen() {
         )
     )
 
-    if ( !activitiesData ) {
-        return <Loading />
-    }
-    else {
+    useEffect( () => {
+        if ( activitiesData ) {
 
-        let monData = activitiesData.filter( item => item.WeekDay === 'Monday' )
-        let tuesData = activitiesData.filter( item => item.WeekDay === 'Tuesday' )
-        let wedData = activitiesData.filter( item => item.WeekDay === 'Wednesday' )
-        let thurData = activitiesData.filter( item => item.WeekDay === 'Thursday' )
-        let friData = activitiesData.filter( item => item.WeekDay === 'Friday' )
-        let satData = activitiesData.filter( item => item.WeekDay === 'Saturday' )
-        let sunData = activitiesData.filter( item => item.WeekDay === 'Sunday' )
+            setTimeout( () => {
+                sectionList.current.scrollToLocation( {
+                    animated: true,
+                    itemIndex: 0,
+                    sectionIndex: day - 1
+                } )
+            }, 300 )
+        }
+    }, [ activitiesData ] )
 
-        //sorting algorithm for chronologically ordering activities
+    if ( !activitiesData ) return <Loading />
 
-        const weeksData = [ monData, tuesData, wedData, thurData, friData, satData, sunData ]
-        //sort by hour
-        weeksData.forEach( dayData => {
-            dayData.sort( ( a, b ) => {
-                let timeA = a.Time.split( ':' )
-                timeA = +timeA[ 0 ]
-                let timeB = b.Time.split( ':' )
-                timeB = +timeB[ 0 ]
+    let monData = activitiesData.filter( item => item.WeekDay === 'Monday' )
+    let tuesData = activitiesData.filter( item => item.WeekDay === 'Tuesday' )
+    let wedData = activitiesData.filter( item => item.WeekDay === 'Wednesday' )
+    let thurData = activitiesData.filter( item => item.WeekDay === 'Thursday' )
+    let friData = activitiesData.filter( item => item.WeekDay === 'Friday' )
+    let satData = activitiesData.filter( item => item.WeekDay === 'Saturday' )
+    let sunData = activitiesData.filter( item => item.WeekDay === 'Sunday' )
 
-                return timeA - timeB;
-            } )
+    //sorting algorithm for chronologically ordering activities
+
+    const weeksData = [ monData, tuesData, wedData, thurData, friData, satData, sunData ]
+    //sort by hour
+    weeksData.forEach( dayData => {
+        dayData.sort( ( a, b ) => {
+            let timeA = a.Time.split( ':' )
+            timeA = +timeA[ 0 ]
+            let timeB = b.Time.split( ':' )
+            timeB = +timeB[ 0 ]
+
+            return timeA - timeB;
         } )
-        //sort by minutes
-        weeksData.forEach( dayData => {
-            dayData.sort( ( a, b ) => {
-                let timeA = a.Time.split( ':' )
-                timeA = +timeA[ 1 ]
-                let timeB = b.Time.split( ':' )
-                timeB = +timeB[ 1 ]
+    } )
+    //sort by minutes
+    weeksData.forEach( dayData => {
+        dayData.sort( ( a, b ) => {
+            let timeA = a.Time.split( ':' )
+            timeA = +timeA[ 1 ]
+            let timeB = b.Time.split( ':' )
+            timeB = +timeB[ 1 ]
 
-                return timeA - timeB;
-            } )
+            return timeA - timeB;
         } )
+    } )
 
 
 
-        const activityData = [
-            { title: 'Monday', data: monData },
-            { title: 'Tuesday', data: tuesData },
-            { title: 'Wednesday', data: wedData },
-            { title: 'Thursday', data: thurData },
-            { title: 'Friday', data: friData },
-            { title: 'Saturday', data: satData },
-            { title: 'Sunday', data: sunData },
-        ]
+    const activityData = [
+        { title: 'Monday', data: monData },
+        { title: 'Tuesday', data: tuesData },
+        { title: 'Wednesday', data: wedData },
+        { title: 'Thursday', data: thurData },
+        { title: 'Friday', data: friData },
+        { title: 'Saturday', data: satData },
+        { title: 'Sunday', data: sunData },
+    ]
 
-        return (
-            <SafeAreaView>
-                <SectionList
-                    sections={ activityData }
-                    renderItem={ ( { item } ) => <ActivityComponent data={ item } /> }
-                    renderSectionHeader={ ( { section } ) => <ActivityDay heading={ section.title } /> }
-                    keyExtractor={ item => item.ActivityID }
-                    stickySectionHeadersEnabled
-                >
+    return (
+        <SafeAreaView>
+            <SectionList
+                sections={ activityData }
+                renderItem={ ( { item } ) => <ActivityComponent data={ item } /> }
+                renderSectionHeader={ ( { section } ) => <ActivityDay heading={ section.title } /> }
+                keyExtractor={ item => item.ActivityID }
+                stickySectionHeadersEnabled
+                ref={ sectionList }
+            >
 
-                </SectionList>
-            </SafeAreaView>
+            </SectionList>
+        </SafeAreaView >
 
-        )
-    }
+    )
 }
+
 
