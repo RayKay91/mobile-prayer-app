44ef915e798cfe11873b3c6700981e0230d03809
Ray Khalid <rkhalid@me.com>
2021-05-23 19:06:02 +0100

    fix font styling for web view on home screen

--- components/webViewContainer.js
+++ components/webViewContainer.js
@@ -2,14 +2,13 @@ import React from 'react'
 import { StyleSheet } from 'react-native'
 import AutoHeightWebView from 'react-native-autoheight-webview'
 
-const WebViewContainer = ( { text, title } ) => {
-    return (
-
-        <AutoHeightWebView
-            style={ { width: '100%' } }
-            androidHardwareAccelerationDisabled
-            customStyle={ `
-                h1,h2,h3,h4,h5,h6,p, .title { font-family: Arial, Helvetica, sans-serif; color: #333}
+const WebViewContainer = ({ text, title }) => {
+  return (
+    <AutoHeightWebView
+      style={{ width: '100%' }}
+      androidHardwareAccelerationDisabled
+      customStyle={`
+                * { font-family: Arial, Helvetica, sans-serif !important; color: #333}
                 .title { font-weight: bold; color: rgb(161, 43, 110)  }
                 a {color:rgb(161, 43, 110)  }
                 
@@ -28,40 +27,45 @@ const WebViewContainer = ( { text, title } ) => {
                 }
                 
                 `}
-            scrollEnabled={ false }
-            // bounces={ false }
-            scrollEnabledWithZoomedin={ true }
-            containerStyle={ [ styles.container, { marginBottom: 40, width: '95%', overflow: 'hidden' } ] }
-            viewportContent={ 'width=device-width' }
-            originWhitelist={ [ 'https://wise-web.org/*' ] }
-            source={ { html: '<meta name="viewport" content="width=device-width, initial-scale=1">' + `<p class="title">${ title }</p>` + '\n\n' + text } }
-        />
-    );
+      scrollEnabled={false}
+      // bounces={ false }
+      scrollEnabledWithZoomedin={true}
+      containerStyle={[
+        styles.container,
+        { marginBottom: 40, width: '95%', overflow: 'hidden' },
+      ]}
+      viewportContent={'width=device-width'}
+      originWhitelist={['https://wise-web.org/*']}
+      source={{
+        html:
+          '<meta name="viewport" content="width=device-width, initial-scale=1">' +
+          `<p class="title">${title}</p>` +
+          '\n\n' +
+          text,
+      }}
+    />
+  )
 }
-
-
-
 
 export default WebViewContainer
 
-const styles = StyleSheet.create( {
-    container: {
-        borderWidth: 2,
-        borderColor: 'rgb(161, 43, 110)',
-        borderRadius: 5,
-        padding: 15,
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        backgroundColor: 'white',
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.23,
-        shadowRadius: 2.62,
-
-        elevation: 4,
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 2,
+    borderColor: 'rgb(161, 43, 110)',
+    borderRadius: 5,
+    padding: 15,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    backgroundColor: 'white',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
 
-} )
+    elevation: 4,
+  },
+})
