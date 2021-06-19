92aac3cffc65688addcf1c551f77f86d74a68f8b
Rayhaan Khalid <rkhalid@me.com>
2021-06-19 15:22:33 +0100

    fix isha time not correctly formatting when >= 11

--- utils/formatTime.js
+++ utils/formatTime.js
@@ -1,25 +1,26 @@
-function formatTime( prayerName, time ) {
+function formatTime(prayerName, time) {
   //check if hour is 12 or 11 if so do not add 12
 
+  if (time.startsWith('12') || (time.startsWith('11') && prayerName === 'Isha'))
+    return time
 
-  if ( time.substring( 0, 2 ) === "12" || time.substring( 0, 2 ) === "11" ) return time;
-
-
-  if ( time.length < 5 && time[ 0 ] !== "0" ) {
-    time = "0" + time;
+  if (time.length < 5 && time[0] !== '0') {
+    time = '0' + time
   }
 
   // cancel 24 hour conversion for fajr and sunrise
-  if ( prayerName === 'Fajr' || prayerName === 'FajrJam' || prayerName === 'Sunrise' ) {
+  if (
+    prayerName === 'Fajr' ||
+    prayerName === 'FajrJam' ||
+    prayerName === 'Sunrise'
+  ) {
     return time
   }
 
+  let hours = time.substring(0, 2)
 
-  let hours = time.substring( 0, 2 );
-
-  hours = parseInt( hours ) + 12;
-  return hours + time.substring( 2 );
-
+  hours = parseInt(hours) + 12
+  return hours + time.substring(2)
 }
 
 module.exports = formatTime
