a3197ab101a19b324a3227f452a72a6185340f0b
RayKay91 <61206305+RayKay91@users.noreply.github.com>
2021-07-10 20:32:32 +0100

    rerender section list on activities fetch

--- screens/ActivitiesScreen.js
+++ screens/ActivitiesScreen.js
@@ -1,114 +1,120 @@
-import React, { useState, useCallback, useEffect, useRef } from 'react';
-import { useFocusEffect } from '@react-navigation/native';
+import React, { useState, useCallback, useEffect, useRef } from 'react'
+import { useFocusEffect } from '@react-navigation/native'
 import { SafeAreaView, SectionList, Alert } from 'react-native'
 import ActivityComponent from '../components/activityComponent'
 import Loading from '../components/loading'
-import getActivitiesData from '../utils/getActivitiesData';
+import getActivitiesData from '../utils/getActivitiesData'
 import ActivityDay from '../components/activityDay'
 
-
 export default function ActivitiesScreen() {
-    const [ activitiesData, setActivitiesData ] = useState( [] )
-    const [ refreshing, setRefreshing ] = useState( false )
-    const sectionList = useRef()
-    const day = new Date().getDay()
+  const [activitiesData, setActivitiesData] = useState([])
+  const [refreshing, setRefreshing] = useState(false)
+  const sectionList = useRef()
+  const day = new Date().getDay()
 
-    useFocusEffect(
-        useCallback(
-            () => {
-                ; ( async function () {
-                    try {
-                        const data = await getActivitiesData()
-                        setActivitiesData( data )
+  useFocusEffect(
+    useCallback(() => {
+      ;(async function () {
+        try {
+          const data = await getActivitiesData()
+          setActivitiesData(data)
+        } catch (error) {
+          Alert.alert(
+            'There was an error getting the activities.',
+            'Please check your internet connection and try again.'
+          )
+        }
+      })()
 
-                    } catch ( error ) {
-                        Alert.alert( 'There was an error getting the activities.', 'Please check your internet connection and try again.' )
-                    }
-                } )()
+      return () => autoScroll()
+    }, [])
+  )
 
-                return () => autoScroll()
-            },
-            [],
-        )
-    )
+  function autoScroll() {
+    sectionList.current.scrollToLocation({
+      animated: true,
+      itemIndex: 0,
+      sectionIndex: day === 0 ? 6 : day - 1,
+    })
+  }
 
-    function autoScroll() {
+  if (activitiesData.length === 0) return <Loading />
 
-        sectionList.current.scrollToLocation( {
-            animated: true,
-            itemIndex: 0,
-            sectionIndex: day === 0 ? 6 : day - 1
-        } )
-    }
+  let monData = activitiesData.filter(item => item.WeekDay === 'Monday')
+  let tuesData = activitiesData.filter(item => item.WeekDay === 'Tuesday')
+  let wedData = activitiesData.filter(item => item.WeekDay === 'Wednesday')
+  let thurData = activitiesData.filter(item => item.WeekDay === 'Thursday')
+  let friData = activitiesData.filter(item => item.WeekDay === 'Friday')
+  let satData = activitiesData.filter(item => item.WeekDay === 'Saturday')
+  let sunData = activitiesData.filter(item => item.WeekDay === 'Sunday')
 
+  //sorting algorithm for chronologically ordering activities
 
-    if ( activitiesData.length === 0 ) return <Loading />
+  const weeksData = [
+    monData,
+    tuesData,
+    wedData,
+    thurData,
+    friData,
+    satData,
+    sunData,
+  ]
+  //sort by hour
+  weeksData.forEach(dayData => {
+    dayData.sort((a, b) => {
+      let timeA = a.Time.split(':')
+      timeA = +timeA[0]
+      let timeB = b.Time.split(':')
+      timeB = +timeB[0]
 
-    let monData = activitiesData.filter( item => item.WeekDay === 'Monday' )
-    let tuesData = activitiesData.filter( item => item.WeekDay === 'Tuesday' )
-    let wedData = activitiesData.filter( item => item.WeekDay === 'Wednesday' )
-    let thurData = activitiesData.filter( item => item.WeekDay === 'Thursday' )
-    let friData = activitiesData.filter( item => item.WeekDay === 'Friday' )
-    let satData = activitiesData.filter( item => item.WeekDay === 'Saturday' )
-    let sunData = activitiesData.filter( item => item.WeekDay === 'Sunday' )
+      return timeA - timeB
+    })
+  })
+  //sort by minutes
+  weeksData.forEach(dayData => {
+    dayData.sort((a, b) => {
+      let timeA = a.Time.split(':')
+      timeA = +timeA[1]
+      let timeB = b.Time.split(':')
+      timeB = +timeB[1]
 
-    //sorting algorithm for chronologically ordering activities
+      return timeA - timeB
+    })
+  })
 
-    const weeksData = [ monData, tuesData, wedData, thurData, friData, satData, sunData ]
-    //sort by hour
-    weeksData.forEach( dayData => {
-        dayData.sort( ( a, b ) => {
-            let timeA = a.Time.split( ':' )
-            timeA = +timeA[ 0 ]
-            let timeB = b.Time.split( ':' )
-            timeB = +timeB[ 0 ]
+  const activityData = [
+    { title: 'Monday', data: monData },
+    { title: 'Tuesday', data: tuesData },
+    { title: 'Wednesday', data: wedData },
+    { title: 'Thursday', data: thurData },
+    { title: 'Friday', data: friData },
+    { title: 'Saturday', data: satData },
+    { title: 'Sunday', data: sunData },
+  ]
 
-            return timeA - timeB;
-        } )
-    } )
-    //sort by minutes
-    weeksData.forEach( dayData => {
-        dayData.sort( ( a, b ) => {
-            let timeA = a.Time.split( ':' )
-            timeA = +timeA[ 1 ]
-            let timeB = b.Time.split( ':' )
-            timeB = +timeB[ 1 ]
-
-            return timeA - timeB;
-        } )
-    } )
-
-
-
-    const activityData = [
-        { title: 'Monday', data: monData },
-        { title: 'Tuesday', data: tuesData },
-        { title: 'Wednesday', data: wedData },
-        { title: 'Thursday', data: thurData },
-        { title: 'Friday', data: friData },
-        { title: 'Saturday', data: satData },
-        { title: 'Sunday', data: sunData },
-    ]
-
-    return (
-        <SafeAreaView>
-            <SectionList
-                sections={ activityData }
-                renderItem={ ( { item } ) => <ActivityComponent data={ item } /> }
-                renderSectionHeader={ ( { section } ) => <ActivityDay heading={ section.title } /> }
-                keyExtractor={ item => item.ActivityID }
-                stickySectionHeadersEnabled
-                ref={ sectionList }
-                onScrollToIndexFailed={ () => Alert.alert( 'These are the current activities' ) }
-                onLayout={ () => setTimeout( autoScroll, 225 ) }
-                onRefresh={ async () => { setRefreshing( true ); const data = await getActivitiesData(); setActivitiesData( data ); setRefreshing( false ) } }
-                refreshing={ refreshing }
-            >
-
-            </SectionList>
-        </SafeAreaView >
-
-    )
+  return (
+    <SafeAreaView>
+      <SectionList
+        sections={activityData}
+        renderItem={({ item }) => <ActivityComponent data={item} />}
+        renderSectionHeader={({ section }) => (
+          <ActivityDay heading={section.title} />
+        )}
+        keyExtractor={item => item.ActivityID}
+        stickySectionHeadersEnabled
+        ref={sectionList}
+        onScrollToIndexFailed={() =>
+          Alert.alert('These are the current activities')
+        }
+        onRefresh={async () => {
+          setRefreshing(true)
+          const data = await getActivitiesData()
+          setActivitiesData(data)
+          setRefreshing(false)
+        }}
+        refreshing={refreshing}
+        extraData={activitiesData}
+      ></SectionList>
+    </SafeAreaView>
+  )
 }
-
-
