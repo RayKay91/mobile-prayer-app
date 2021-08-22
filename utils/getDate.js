0db697830ac4e489bc7b1daf0d1956f068640080
RayKay91 <61206305+RayKay91@users.noreply.github.com>
2021-08-22 21:50:52 +0100

    colorise date if wrong + use date from api

--- utils/getDate.js
+++ utils/getDate.js
@@ -1,48 +1,40 @@
-const dayjs = require( 'dayjs' )
+import AsyncStorage from '@react-native-async-storage/async-storage'
+const dayjs = require('dayjs')
+const customParseFormat = require('dayjs/plugin/customParseFormat')
+dayjs.extend(customParseFormat)
 
+async function getDate() {
+  const gregorianDateFromApi = await AsyncStorage.getItem('gregorianDate')
 
-function getDate( showTmrwDate ) {
-    let date = dayjs();
-    const gregorianDate = dayjs().format( 'dddd D MMM YYYY' );
-
-    if ( showTmrwDate ) {
-        date = dayjs().add( 1, 'd' );
-        return date.format( 'dddd D MMM YYYY' );
-    }
-    return gregorianDate;
+  let date = dayjs(gregorianDateFromApi, 'YYYY/MM/DD')
+  return {
+    today: date.format('dddd D MMMM YYYY'),
+    tomorrow: date.add(1, 'd').format('dddd D MMMM YYYY'),
+  }
 }
 
-function calculateHijriDate( { StartDate, Month, Year } ) {
+function calculateHijriDate({ StartDate, Month, Year }) {
+  let currentDate = dayjs().format('YYYY-MM-DD')
+  currentDate = dayjs(currentDate)
+  let elapsedHijriDays = currentDate.diff(StartDate, 'day')
+  //adding 1 to the difference calculated to account for impossibility of 0/mm/yyyy
+  elapsedHijriDays += 1
 
-    let currentDate = dayjs().format( 'YYYY-MM-DD' )
-    currentDate = dayjs( currentDate )
-    let elapsedHijriDays = currentDate.diff( StartDate, 'day' )
-    //adding 1 to the difference calculated to account for impossibility of 0/mm/yyyy
-    elapsedHijriDays += 1
+  if (+elapsedHijriDays > 30) return ''
 
-    if (+elapsedHijriDays > 30 ) return ''
-
-
-    return `${ elapsedHijriDays } ${ Month } ${ Year } AH`;
+  return `${elapsedHijriDays} ${Month} ${Year} AH`
 }
 
-function tmrwHijriDate( currHijriDate ) {
+function tmrwHijriDate(currHijriDate) {
+  let [date, month, ...year] = currHijriDate.split(' ')
 
-    let [ date, month, ...year ] = currHijriDate.split( ' ' );
+  date = +date + 1
 
+  if (date > 30) return ''
 
-    date = +date + 1;
+  const dateElements = [date, month, ...year]
 
-    if ( date > 30 ) return ''
-
-    const dateElements = [ date, month, ...year ];
-
-    return dateElements.join( ' ' );
+  return dateElements.join(' ')
 }
-
-
-
-
-
 
 module.exports = { getDate, calculateHijriDate, tmrwHijriDate }
